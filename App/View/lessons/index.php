b875ca531d2891e944d903ed2091c8c8ea4219c5
mathieugeissler <contact@mathieugeissler.fr>
2015-10-05 14:31:13 +0200

    Users/Profil - Lessons/Index - Users/Index

--- App/View/lessons/index.php
+++ App/View/lessons/index.php
@@ -1,3 +1,18 @@
-<div>
-	<h1> la page des leçons</h1>
-</div>
+	<div class="container">
+		<div class="panel panel-default">
+			<div class="panel-body text-center sequence">
+				<h1 class="iconfull"><span class="glyphicon glyphicon-volume-up"></span></h1>
+				<audio controls="controls" style="width: 500px;">
+  Votre navigateur ne supporte pas l'élément <code>audio</code>.
+  <source src="toto.wav" type="audio/wav">
+</audio>
+			</div>
+			<div class="progress fullheight">
+				<div class="progress-bar progheight" role="progressbar" aria-valuenow="60"
+					aria-valuemin="0" aria-valuemax="100" style="width: 60%;">6/10 briques</div>
+				<div class="text-right">
+					<button class="btn-sm btn-success">Suivant</button>
+				</div>
+			</div>
+		</div>
+	</div>
