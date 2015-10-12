c1e5a076afca46c03b888a3009d1d1b75f4c3d49
mathieugeissler <contact@mathieugeissler.fr>
2015-10-12 15:48:42 +0200

    edit bricks

--- App/View/bricks/edit.php
+++ App/View/bricks/edit.php
@@ -1,44 +1,96 @@
-<form class="form-horizontal" method="post">
-    <fieldset>
+<div class="container contcolor">
+    <div id="deco_blanc3"></div>
+    <div class="col-sm-12 cadre">
+        <div class="entete"></div>
+        <div class="panel">
+            <div class="panel-heading enteteFlou">
+                <h1>BRIQUES</h1>
 
-        <!-- Form Name -->
-        <legend>Form Name</legend>
+                <div class="cercle">
+                    <img
+                        src="<?php echo URL ?>public/img/connexion.png"
+                        alt="logo" height="63px" width="71px"
+                        style="top: 18px; position: relative; left: 12px"/>
+                </div>
+            </div>
+            <div class="panel-body panelCategorie panelSession">
 
-        <!-- Select Basic -->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="selectbasic">Select Basic</label>
-            <div class="col-md-4">
-                <select id="selectbasic" name="selectbasic" class="form-control">
-                    <option value="1">Option one</option>
-                    <option value="2">Option two</option>
-                </select>
+                <div class="formulaire">
+                    <form class="form-horizontal" method="post">
+                        <fieldset>
+
+                            <div class="col-md-6">
+                                <!-- Text input-->
+                                <div class="form-group editBricks">
+                                    <select id="bricks[type]" name="bricks[type]" class="form-control">
+                                        <option value="1">Brique 1</option>
+                                        <option value="2">Brique 2</option>
+                                        <option value="2">Brique 3</option>
+                                        <option value="2">Brique 4</option>
+                                        <option value="2">Brique 5</option>
+                                    </select>
+                                </div>
+                                <div class="form-group editBricks"">
+                                    <input id="nameInput" name="bricks[name]" type="text" placeholder="Nom de la brique"
+                                           class="floating-label form-control input-md">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group editBricks"">
+                                    <input id="nameInput" name="bricks[data]" type="text"
+                                           placeholder="Text de la brique" class="floating-label form-control input-md">
+                                </div>
+                                <div class="form-group editBricks"">
+                                    <input id="filebutton" name="filebutton" class="btn btn-primary btn-sm" type="file">
+                                </div>
+                            </div>
+                            <!-- Button (Double) -->
+                            <div class="form-group text-center">
+                                <button type="submit" id="button1id" name="button1id" class="btn btn-info">
+                                    Sauvegarder
+                                </button>
+                            </div>
+
+                        </fieldset>
+                    </form>
+                    <table class="table table-striped table-hover ">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nom</th>
+                            <th>Type</th>
+                            <th>Média</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Brique 1</td>
+                            <td>MIDI</td>
+                            <td>fichier.midi</td>
+                            <td>
+                                <button class="btn btn-info btn-sm">Editer</button>
+                                <button class="btn btn-danger btn-sm">Supprimer</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Brique 2</td>
+                            <td>TTS</td>
+                            <td>TEXT TEXT TEXT TEXT</td>
+                            <td>
+                                <button class="btn btn-info btn-sm">Editer</button>
+                                <button class="btn btn-danger btn-sm">Supprimer</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div class="text-right">
+                        <button id="button2id" name="button2id" class="btn btn-success">Valider</button>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <!-- Text input-->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="textinput">Text Input</label>
-            <div class="col-md-4">
-                <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
-                <span class="help-block">help</span>
-            </div>
-        </div>
-
-        <!-- File Button -->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="filebutton">File Button</label>
-            <div class="col-md-4">
-                <input id="filebutton" name="filebutton" class="input-file" type="file">
-            </div>
-        </div>
-
-        <!-- Button -->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="singlebutton">Single Button</label>
-            <div class="col-md-4">
-                <button type="submit" id="singlebutton" name="singlebutton" class="btn btn-primary">Sauvegarder</button>
-            </div>
-        </div>
-
-    </fieldset>
-</form>
+    </div>
+</div>
