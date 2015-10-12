c8f48e383458021a1b647b824f1c75670e195bdf
mathieugeissler <contact@mathieugeissler.fr>
2015-10-12 15:55:53 +0200

    ON DELETE
    # Conflicts:
    #	App/Controller/UsersController.php
    #	App/Core/Bootstrap.php
    #	App/Core/Controller.php
    #	App/Core/Database.php
    #	App/Core/Model.php
    #	App/Core/View.php
    #	App/Model/User.php
    #	App/View/footer.php
    #	App/View/header.php
    #	App/View/lessons/index.php
    #	App/View/navbar.php
    #	App/View/users/register.php
    #	config.php
    #	public/css/dev.css
    #	public/img/fond_inscription.jpg
    #	public/img/new_membre.png

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('URL', 'http://192.168.33.10/LAVAL/');
-define('LIBS', 'App/Core/');
-define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'laval');
-define('DB_USER', 'root');
-define('DB_PASS', '');
