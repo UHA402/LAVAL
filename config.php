499af336c272370ee494bdf77921541b7d5d0d5e
Vincent <vincnet.pierrat@gmail.com>
2015-10-14 14:27:18 +0200

    Added htaccess and config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+define('URL', 'http://localhost/');
+define('LIBS', 'App/Core/');
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'laval');
+define('DB_USER', 'root');
+define('DB_PASS', '');
