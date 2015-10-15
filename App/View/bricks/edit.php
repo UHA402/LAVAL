e807190cb7d92a4358613a447721274cb6816b90
mathieugeissler <contact@mathieugeissler.fr>
2015-10-15 11:44:54 +0200

    Update Sessions and Sequ edit view

--- App/View/bricks/edit.php
+++ App/View/bricks/edit.php
@@ -30,67 +30,70 @@
                                         <option value="2">Brique 5</option>
                                     </select>
                                 </div>
-                                <div class="form-group editBricks"">
-                                    <input id="nameInput" name="bricks[name]" type="text" placeholder="Nom de la brique"
-                                           class="floating-label form-control input-md">
-                                </div>
+                                <div class="form-group editBricks"
+                                ">
+                                <input id="nameInput" name="bricks[name]" type="text" placeholder="Nom de la brique"
+                                       class="floating-label form-control input-md">
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group editBricks"">
-                                    <input id="nameInput" name="bricks[data]" type="text"
-                                           placeholder="Text de la brique" class="floating-label form-control input-md">
-                                </div>
-                                <div class="form-group editBricks"">
-                                    <input id="filebutton" name="filebutton" class="btn btn-primary btn-sm" type="file">
-                                </div>
-                            </div>
-                            <!-- Button (Double) -->
-                            <div class="form-group text-center">
-                                <button type="submit" id="button1id" name="button1id" class="btn btn-info">
-                                    Sauvegarder
-                                </button>
-                            </div>
-
-                        </fieldset>
-                    </form>
-                    <table class="table table-striped table-hover ">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Nom</th>
-                            <th>Type</th>
-                            <th>Média</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Brique 1</td>
-                            <td>MIDI</td>
-                            <td>fichier.midi</td>
-                            <td>
-                                <button class="btn btn-info btn-sm">Editer</button>
-                                <button class="btn btn-danger btn-sm">Supprimer</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Brique 2</td>
-                            <td>TTS</td>
-                            <td>TEXT TEXT TEXT TEXT</td>
-                            <td>
-                                <button class="btn btn-info btn-sm">Editer</button>
-                                <button class="btn btn-danger btn-sm">Supprimer</button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                    <div class="text-right">
-                        <button id="button2id" name="button2id" class="btn btn-success">Valider</button>
-                    </div>
                 </div>
+                <div class="col-md-6">
+                    <div class="form-group editBricks"
+                    ">
+                    <input id="nameInput" name="bricks[data]" type="text"
+                           placeholder="Text de la brique" class="floating-label form-control input-md">
+                </div>
+                <div class="form-group editBricks"
+                ">
+                <input id="filebutton" name="filebutton" class="btn btn-primary btn-sm" type="file">
             </div>
         </div>
+        <!-- Button (Double) -->
+        <div class="form-group text-center">
+            <button type="submit" id="button1id" name="button1id" class="btn btn-info">
+                Sauvegarder
+            </button>
+        </div>
+
+        </fieldset>
+        </form>
+        <table class="table table-striped table-hover ">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Nom</th>
+                <th>Type</th>
+                <th>Média</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>Brique 1</td>
+                <td>MIDI</td>
+                <td>fichier.midi</td>
+                <td>
+                    <button class="btn btn-info btn-sm">Editer</button>
+                    <button class="btn btn-danger btn-sm">Supprimer</button>
+                </td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Brique 2</td>
+                <td>TTS</td>
+                <td>TEXT TEXT TEXT TEXT</td>
+                <td>
+                    <button class="btn btn-info btn-sm">Editer</button>
+                    <button class="btn btn-danger btn-sm">Supprimer</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="text-right">
+            <button id="button2id" name="button2id" class="btn btn-success">Valider</button>
+        </div>
     </div>
+</div>
+</div>
+</div>
 </div>
