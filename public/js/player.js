45651d10f20fe708638f59761688b9d674c24f47
mathieugeissler <contact@mathieugeissler.fr>
2015-10-27 20:02:17 +0100

    set result into brick

--- public/js/player.js
+++ public/js/player.js
@@ -3,9 +3,11 @@ $(function () {
     //DEBUG
     var brick1 = {id: 1, name: "Brick 1", type: "TTS", data: "text to speech"};
     var brick2 = {id: 2, name: "Brick 2", type: "TXT", data: "un,deux,trois,quatre"};
-    var brick3 = {id: 3, name: "Brick 3", type: "IMG", data: "http://img.fr"};
-    var brick4 = {id: 4, name: "Brick 4", type: "Wave", data: "http://sound.fr"};
-    var brickList = [brick1, brick2, brick3, brick4];
+    //var brick3 = {id: 3, name: "Brick 3", type: "IMG", data: "http://img.fr"};
+    var brick4 = {id: 4, name: "Brick 4", type: "WAVE", data: "http://sound.fr"};
+    var brick5 = {id: 1, name: "Brick 1", type: "TTS", data: "text to speech 1"};
+    var brick6 = {id: 1, name: "Brick 1", type: "TTS", data: "text to speech 2"};
+    var brickList = [brick1, brick2, brick4, brick5, brick6];
 
     //Initialisation du player
     var player = new Player(brickList);
@@ -32,55 +34,55 @@ var Player = function (brickList) {
     // Start le Player
     this.start = function () {
         //Charge l'event sur le bouton next
-        $('#next-brique').click(function() {
+        $('#next-brique').click(function () {
             loadBrick();
         });
         //Charge la premiere brick
         loadBrick();
     }
 
+
+    //position dans la list
+    var posList = 0;
+    function incPosList() {
+        posList += 1;
+        console.log('_Position inc : (' + posList + ')');
+    }
+
     // Refresh le status du player
     function refreshStatus() {
         console.log('Status refresh...');
-        //Verification de la fin de la liste
-        if(posList < getNbrBrick())
-        {
-            incPosList();
-            majProgressBar();
-        }  else {
+        incPosList();
+        majProgressBar();
+
+        //fin de la sequence
+        if(posList === getNbrBrick()) {
             $('#next-brique').removeClass("btn-success");
             $('#next-brique').addClass("btn-info");
             $('#next-brique').text("Finish");
             $("#next-brique").off("click");
             $("#next-brique").click(function () {
-                alert("FINI !");
+                sequenceDone();
             });
         }
     }
 
-    //position dans la list
-    var posList = 0;
-    function incPosList() {
-        console.log('_Position inc');
-        posList += 1;
-    }
-
     //Charge une brick
     function loadBrick() {
-        console.log('Load Brick...');
-        $("#next-brique").show();
+        console.log('Load Brick ' + posList + '...');
         brick[posList].loadModel();
         refreshStatus();
     }
 
     //Met à jour la bar de progression
     function majProgressBar() {
-        if(posList === 0) {
-            $(".progress-bar").attr("style", "width: 0%");
-        } else {
-            var prog = ((posList) * getNbrBrick());
+            var prog = (posList / getNbrBrick()) * 100;
             $(".progress-bar").attr("style", "width: " + prog + "%");
-        }
+    }
+
+    //Fin de la séquence envoi des données au controller PHP
+    function sequenceDone() {
+
     }
 }
 
@@ -92,14 +94,19 @@ var Brick = function (value) {
     this.result;
 
     this.loadModel = function () {
-        var model = new Model(this.type, this.data);
+        var model = new Model(this);
         console.log('Load Model...')
         return model.load();
     }
+
+    this.setResult = function (value) {
+        this.result = value;
+    }
 }
 
-var Model = function (type, data) {
-
+var Model = function (brick) {
+    var type = brick.type;
+    var data = brick.data;
     this.load = function () {
         if (type === 'TTS') {
             console.log('Model TTS Loaded');
@@ -120,17 +127,36 @@ var Model = function (type, data) {
 
         } else if (type === 'TXT') {
             console.log('Model TXT Loaded');
+            //cache le bouton next;
+            $("#next-brique").hide();
             //split la chaine de caractere
             var stimu = data.split(",");
             //insert les stimuli dans le model
             $("#briqueContent").load(url + "player/txtLayout/", function () {
-                $("#stimu1").append('<a id="repStimu1" href="#"><h1>' + stimu[0] + '</h1></a>')
-                $("#stimu2").append('<a id="repStimu2" href="#"><h1>' + stimu[1] + '</h1></a>')
-                $("#stimu3").append('<a id="repStimu3" href="#"><h1>' + stimu[2] + '</h1></a>')
-                $("#stimu4").append('<a id="repStimu4" href="#"><h1>' + stimu[3] + '</h1></a>')
+                for (var i = 1; i < 5; i++) {
+                    $("#stimu" + i).append('<a id="repStimu' + i + '" href="javascript:void(0)"><h1>' + stimu[i - 1] + '</h1></a>');
+                }
+                $("#repStimu1").click(function () {
+                    brick.setResult(stimu[0]);
+                    console.log('Result : ' + brick.result + ' saved');
+                    $("#next-brique").show();
+                });
+                $("#repStimu2").click(function () {
+                    brick.setResult(stimu[1]);
+                    console.log('Result : ' + brick.result + ' saved');
+                    $("#next-brique").show();
+                });
+                $("#repStimu3").click(function () {
+                    brick.setResult(stimu[2]);
+                    console.log('Result : ' + brick.result + ' saved');
+                    $("#next-brique").show();
+                });
+                $("#repStimu4").click(function () {
+                    brick.setResult(stimu[3]);
+                    console.log('Result : ' + brick.result + ' saved');
+                    $("#next-brique").show();
+                });
             });
-
-            $("#next-brique").hide();
         }
     }
 }
